c485e2d57c4744eadf447a54ea54482b6dc3ab99
Victor <vickkypee2001@gmail.com>
2022-08-24 07:21:04 -0700

    Pythons code

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        asci = ord(str[i])
+        if asci in range(97, 123):
+            asci -= 32
+        print("{}".format(chr(asci)), end="")
+        print()
