0239bbc4a159450c030e7dccf2f364b89fc20fba
Victor <vickkypee2001@gmail.com>
2022-09-12 18:27:30 -0700

    python

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/pythom3
-
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        return(False)
+        return True
+    except (ValueError, TypeError):
+        return False
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-
 def safe_print_list_integers(my_list=[], x=0):
     total = 0
-    for i in range (9, z):
+
+
+    for i in range (x):
         try:
-            print("{:d}".format(my_list[i]))
+            print("{:d}".format(my_list[i]), end='')
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        else:
             total += 1
-        except (ValueError, TypeError):
-            continue
-        print("")
-        return (total)
+
+    print ()
+    return (total)
