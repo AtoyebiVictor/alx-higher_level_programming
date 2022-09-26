09c866b3ee9aae61a6a92cbfaa900a74404973b1
Victor <105756508+AtoyebiVictor@users.noreply.github.com>
2022-09-27 00:50:45 +0100

    python inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -6,8 +6,3 @@ def lookup(obj):
     """ function that returns the list of available
     attributes and methods of an object """
     return (dir(obj))
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
